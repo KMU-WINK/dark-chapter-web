059852f05c46bf0316176bae5a83018cf88261fb
H0zzae <39540561+H0zzae@users.noreply.github.com>
2021-08-15 23:21:08 +0900

    1차 입력폼 완성

--- src/pages/applock.js
+++ src/pages/applock.js
@@ -8,17 +8,22 @@ function AppLock (){
         <div className='passwordCircle'> </div>,
         <div className='passwordCircle'> </div>,
         <div className='passwordCircle'> </div>]);
+    const [notice, setNotice] = useState('앱을 잠그기 위한 비밀번호를 입력해 주세요.')
     const inputNumber = ({target}) =>{
         setPassword(target.value)
 
     }
     return(
         <div className='root'>
-            <label htmlFor="password">passWord</label>
+            <label htmlFor="password"className='appLockTitle'>앱 비밀번호 설정</label>
+            <label htmlFor="password" className='lockNotice'>{notice}</label>
             <div className='passwordSection'>
-                {passwordComponent}
+                {password[0]?<div className='passwordFill'>{password[0]}</div>:<div className='passwordCircle'>{null}</div>}
+                {password[1]?<div className='passwordFill'>{password[1]}</div>:<div className='passwordCircle'>{null}</div>}
+                {password[2]?<div className='passwordFill'>{password[2]}</div>:<div className='passwordCircle'>{null}</div>}
+                {password[3]?<div className='passwordFill'>{password[3]}</div>:<div className='passwordCircle'>{null}</div>}
+                <input type="text" id='password' maxLength='4' onChange={inputNumber} />
             </div>
-            <input type="text" id='password' maxLength='4' onChange={inputNumber}/>
         </div>
     )
 };
