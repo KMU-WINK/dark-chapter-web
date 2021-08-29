da78b8bc60be8dba09f3938734724984fd36f402
Youngho <cyh6099@gmail.com>
2021-08-29 21:00:54 +0900

    input 넣었습니다 ㅎㅎ

--- src/component/input/EmailInput.js
+++ src/component/input/EmailInput.js
@@ -1,36 +1,107 @@
 import styled from "styled-components";
+import React, { useState, useEffect } from 'react';
 
-import React from "react";
-import goBackBtn from "../../svg/goBackBtn.svg"
 
-import {useHistory} from "react-router-dom";
+import SuccessMessage from "../message/SuccessMessage";
+import ErrorMessage from "../message/ErrorMessage";
 
-function GoBackBtn(props){
-    const history = useHistory();
+import emailIcon from '../../svg/mail.svg'
+import bi_x_active from '../../svg/bi_x.svg'
+import bi_x_inactive from '../../svg/bi_x_inactive.svg'
 
-    console.log(props)
-    return(
-        <Div>
-            <BackIcon onClick={() =>{
-                history.push(props.previousPage)
-            }}/>
-        </Div>
+
+function EmailInput(props){
+    const [email, setEmail] = useState("");
+
+    // @여부에 따라 input border 색깔 변경
+    const [isAtSign, setIsAtSign] = useState()
+
+
+    const onChangeEmail = e => {
+        setEmail(e.target.value);
+    };
+
+
+    useEffect(()=>{
+        if(email.indexOf('@') > -1){
+            setIsAtSign(true);
+            props.setIsActive(true);
+        }
+        else {
+            setIsAtSign(false);
+            props.setIsActive(false)
+        }
+        props.setEmail(email)
+
+    },[email])
+
+    return (
+        <Wrap>
+
+            <InputBox>
+                <img
+                    src={emailIcon}
+                    style={{
+                        position : 'absolute',
+                        left: 8,
+                        top: 8
+                    }}
+                />
+                <InputEmail
+                    onChange={onChangeEmail}
+                    value={email}
+                    active = {isAtSign}
+                    type = "text"
+                />
+
+                {isAtSign
+                    ?<XIcon onClick={() => setEmail("")} />
+                    :<InactiveXIcon onClick={() => setEmail("")}/>
+                }
+            </InputBox>
+            <ErrorMessage message ={props.message} />
+
+        </Wrap>
     )
 }
-
-const Div = styled.div`
-    width : 100%;
-    height : 56px;
+const Wrap = styled.div`
+    width: 328px;
+    margin : 0 auto;
+    margin-top : 88px;
+    position : relative;
+`
+const InputBox = styled.div`
+    height: 38px;
     position : relative;
 `
 
-const BackIcon = styled.img.attrs({
-    src : goBackBtn
+const InputEmail = styled.input.attrs({
+    placeholder:"BlackYeoksa@gmail.com"
 })`
-    position:absolute;
-    top:50%;
-    transform:translateY(-50%);
-    left : 24px
+    text-align : center;
+    width: 328px;
+    height: 38px;
+    outline: 0;
+    border-width: 0 0 2px;
+    border-color: #828282;
+    font-size: 14px;
+    background : #2C2C38;
+    color : #FFFFFF;
+    border-color: ${props => props.active ? "#FFFFFF" : "#828282"}
+`
+const XIcon = styled.img.attrs({
+    src : bi_x_active
+})`
+    position : absolute;
+    left: 304px;
+    top: 7px;
+`
+const InactiveXIcon = styled.img.attrs({
+    src : bi_x_inactive
+})`
+    position : absolute;
+    left: 304px;
+    top: 7px;
 `
 
-export default GoBackBtn
+export default EmailInput
