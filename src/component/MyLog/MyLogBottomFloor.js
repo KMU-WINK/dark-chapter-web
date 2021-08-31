b7dc4b55a17bfa2c046a827af24363ad2400af3f
Youngho <cyh6099@gmail.com>
2021-08-31 14:44:16 +0900

    My-log page(눈금, 그라디언트 구현 필요)

---
+++ src/component/MyLog/MyLogBottomFloor.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from "styled-components";
+
+import fish from "../../svg/fish.svg"
+
+function MyLogBottomFloor(props){
+    return(
+        <Div background={props.background}>
+            <Text color={props.color}>{props.floor}</Text>
+            <Fish></Fish>
+        </Div>
+    )
+}
+
+const Div = styled.div`
+    width : 100%;
+    height : 200px;
+    background : ${props => props.background};
+    margin-top: 180px;
+`
+
+const Text = styled.span`
+    display : block;
+    margin-top : 39px;
+    margin-left : 24px;
+    margin-bottom : -40px;
+    font-size: 16px;
+    line-height: 24px;
+    letter-spacing: -0.03em;
+    font-weight : bold;
+    color: ${props => props.color}
+`
+
+const Fish = styled.img.attrs({
+    src : fish
+})`
+    margin-left : 176px;
+    margin-top : 60px;
+    opacity: 0.3;
+`
+
+export default MyLogBottomFloor
