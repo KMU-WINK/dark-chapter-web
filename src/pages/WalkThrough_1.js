da79b5b1d67d84adda3954010dfbd46c538041ec
Youngho <cyh6099@gmail.com>
2021-09-07 21:42:23 +0900

    walk through거의 완성

--- src/pages/WalkThrough_1.js
+++ src/pages/WalkThrough_1.js
@@ -4,12 +4,13 @@ import styled from "styled-components";
 import background_1 from "../svg/walk_through_1.svg"
 import background_2 from "../svg/walk_through_2.svg";
 import background_3 from "../svg/walk_through_3.svg";
+
 import WalkThoughText from "../component/walk-through/WalkThoughText";
 import Step from "../component/walk-through/Step";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import WalkThrough_2 from "./WalkThrough_2";
+
 
 const WalkThrough_1 = () => {
     const [currentSlide, setSlide] = useState(0)
@@ -40,39 +41,27 @@ const WalkThrough_1 = () => {
 
     const step = [[1,0.5,0.5], [0.5,1,0.5], [0.5,0.5,1]]
 
-
+    const a = "13"
 
     return (
 
         <Container id="qq">
             <Step step={step[currentIdx]}/>
             <Slider {...settings}>
-                {/*<div style={{}}>*/}
-                {/*    <h3>1</h3>*/}
-                {/*</div >*/}
-                {/*<div style={{}}>*/}
-                {/*    <h3>2</h3>*/}
-                {/*</div>*/}
-                {/*<div style={{}}>*/}
-                {/*    <h3>3</h3>*/}
-                {/*</div>*/}
-                <Wrap>
+                <Wrap bg={background_1} id="first">
                     <WalkThoughText text={text1}/>
-                    <div style={{position: "absolute", bottom:0, width: "100%"}}>
-                        <Wave/>
+                    <Img1/>
 
-                    </div>
-                    {/*<Img1/>*/}
                 </Wrap>
 
-                <Wrap>
+                <Wrap bg={background_2}>
                     <WalkThoughText text={text2}/>
-                    {/*<Img2/>*/}
+                    <Img2/>
                 </Wrap>
 
-                <Wrap>
+                <Wrap bg={background_3}>
                     <WalkThoughText text={text3}/>
-                    {/*<Img3/>*/}
+                    <Img3/>
                 </Wrap>
             </Slider>
         </Container>
@@ -80,6 +69,38 @@ const WalkThrough_1 = () => {
     )
 }
 
+const Wrap = styled.div`
+    width: 100%;
+    height: 100vh;
+    position: relative;
+`
+
+const Img1 = styled.img.attrs({
+    src:background_1
+})`
+    width: 100%;
+    
+    position: absolute;
+    bottom:0
+`
+
+const Img2 = styled.img.attrs({
+    src:background_2
+})`
+    width: 100%;
+    
+    position: absolute;
+    bottom:0
+    
+`
+
+const Img3 = styled.img.attrs({
+    src:background_3
+})`
+    width: 100%;
+    position: absolute;
+    bottom: 0;
+`
 
 const Container = styled.div`
     background: #2C2C38;
@@ -87,47 +108,7 @@ const Container = styled.div`
     height: 100vh;
 `
 
-const Wrap = styled(Container)`
-    position: relative;
-`
 
-const Wave = styled.div`
-  position: relative;
-  bottom: 0;
-  height: 300px;
-  width: 100%;
-  background: black;
-  
-  
-  
-  &::before {
-    content: "";
-      display: block;
-      position: absolute;
-      border-radius: 100% 50%;
-      width: 80%;
-      height: 200px;
-      background-color: black;
-      right: -140px;
-      top: -60px;
-      border-top: 1px solid white
-      
-  };
-  
-  &::after {
-    content: "";
-  display: block;
-  position: absolute;
-  border-radius: 60% 50%;
-  width: 90%;
-  height: 240px;
-  background-color: #2C2C38;
-  left: -58px;
-  top: -200px;
-  border-bottom: 1px solid white
-  
-  }
-`
 
 
 
